114a36ac182b445a764f8c595f33a43bca1c2c2e
jike <2397724385qq@.com>
2019-08-01 07:58:53 +0800

    修改文件

---
+++ code/js/shouye.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".daohang").hover(function(){
+        $(".other").css('display','block');
+        $(".other").css('border-top','none');
+    },function(){
+        $(".other").css('display','none');
+      });
+      
+})
