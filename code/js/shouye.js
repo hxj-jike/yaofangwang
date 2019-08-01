ec10be3b8dff29201950c21600818bba5b3f648d
jike <2397724385qq@.com>
2019-08-01 11:59:58 +0800

    上传文件

--- code/js/shouye.js
+++ code/js/shouye.js
@@ -5,5 +5,26 @@ $(function(){
     },function(){
         $(".other").css('display','none');
       });
-      
+      var oca = document.getElementsByClassName("ca");
+    var ocat = document.getElementsByClassName("subcat");
+    function fn() {
+        for (var j = 0; j < oca.length; j++) {
+            oca[j].className = '';
+            ocat[j].style.display = 'none';
+        }
+    }
+    for (i = 0; i < oca.length; i++) {
+        var ca = oca[i];
+        ca.index = i;
+        ca.onmouseover = function () {
+            fn();
+            this.className = 'gh';
+            ocat[this.index].style.display = "block";
+        }
+        ca.onmouseout = function () {
+            fn();
+            ocat[this.index].style.display = "none";
+        }
+    }
+    
 })
